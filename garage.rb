df8383c8bbbc9436fe68c05272863e4d6a21c8fd
Justin Aiken <60tonangel@gmail.com>
2015-12-10 19:45:36 -0700

    Allow ENV var settings

--- garage.rb
+++ garage.rb
@@ -4,7 +4,13 @@ require 'singleton'
 require 'active_support/core_ext/module/delegation.rb'
 require_relative 'my_q'
 
-CONFIG = YAML.load_file 'settings.yml'
+CONFIG   = YAML.load_file 'settings.yml' rescue {'openhab' => {}, 'my_q' => {}}
+USERNAME = (ENV["MYQ_USER"]         || CONFIG['my_q']['user']).freeze
+PASSWORD = (ENV["MYQ_PASS"]         || CONFIG['my_q']['pass']).freeze
+DOOR_ID  = (ENV["MYQ_DOOR_ID"]      || CONFIG['my_q']['door_id']).freeze
+BASE_URL = (ENV["OPENHAB_URL"]      || CONFIG['openhab']['base_url']).freeze
+ITEM     = (ENV["OPENHAB_ITEMNAME"] || CONFIG['openhab']['item_name']).freeze
+HEADERS  = {"Content-Type" => "text/plain"}.freeze
 
 class Garage
   include Singleton
@@ -18,14 +24,11 @@ class Garage
   delegate *%i{status open! close!}, to: :my_q
 
   def initialize
-    @my_q = MyQ::Client.new username: CONFIG['my_q']['user'], password: CONFIG['my_q']['pass'], door_id: CONFIG['my_q']['door_id']
+    @my_q = MyQ::Client.new username: USERNAME, password: PASSWORD, door_id: DOOR_ID
   end
 end
 
 class OpenHABUpdater
-  BASE_URL = CONFIG['openhab']['base_url'].freeze
-  ITEM     = CONFIG['openhab']['item_name'].freeze
-  HEADERS  = {"Content-Type" => "text/plain"}.freeze
 
   def self.update!(status)
     puts "#{BASE_URL}#{ITEM}/state/ = #{status}"
